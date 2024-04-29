83e63f09c009b659e9fcdf362612c6556422a5ce
Wahyu Agus Arifin - IP <68760662+ItPohgero@users.noreply.github.com>
2024-04-29 09:40:18 +0700

    push

---
+++ src/components/ui/container.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Container = ({
+  height,
+  placeholder = 'Template',
+}: {
+  height: string
+  placeholder?: string
+}) => {
+  return (
+    <div className="w-full rounded-xl bg-white/5 p-4 ring-1 ring-inset ring-gray-900/10 dark:ring-white/10 lg:-m-4">
+      <div
+        className={`aspect-w-16 flex items-center justify-center h-[${height}] aspect-h-10 sm:aspect-h-6 lg:aspect-h-4 relative overflow-hidden rounded-lg border border-dashed border-gray-900/10 dark:border-white/10`}
+      >
+        <div className="text-3xl font-bold text-slate-300">{placeholder}</div>
+        <svg
+          className="absolute inset-0 h-full w-full stroke-gray-900/10 dark:stroke-white/10"
+          fill="none"
+        >
+          <defs>
+            <pattern
+              id="pattern-5c1e4f0e-62d5-498b-8ff0-cf77bb448c8e"
+              x="0"
+              y="0"
+              width="10"
+              height="10"
+              patternUnits="userSpaceOnUse"
+            >
+              <path d="M-3 13 15-5M-5 5l18-18M-1 21 17 3" />
+            </pattern>
+          </defs>
+          <rect
+            stroke="none"
+            fill="url(#pattern-5c1e4f0e-62d5-498b-8ff0-cf77bb448c8e)"
+            width="100%"
+            height="100%"
+          />
+        </svg>
+      </div>
+    </div>
+  )
+}
+
+export default Container
--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -33,6 +33,45 @@ const LayoutMain = ({ children }: PropsWithChildren) => {
       <div className="relative z-10">
         <main className="mx-auto max-w-7xl pt-10">{children}</main>
       </div>
+
+      <div className="mt-40">
+        <div className="mx-auto max-w-2xl py-10 text-slate-700">
+          <div className="text-center">
+            <h3 className="mb-3 text-3xl"> Download Docs Template By IT Pohgero </h3>
+            <p> Keep Growing. All day, Every day. </p>
+            <div className="my-10 flex justify-center">
+              <div className="mx-2 flex w-52 items-center rounded-lg border px-4 py-2">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/888/888857.png"
+                  className="w-7 md:w-8"
+                />
+                <div className="ml-3 text-left">
+                  <p className="text-xs text-gray-700">Download on </p>
+                  <p className="text-sm md:text-base"> Google Play Store </p>
+                </div>
+              </div>
+              <div className="mx-2 flex w-44 items-center rounded-lg border px-4 py-2">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/888/888841.png"
+                  className="w-7 md:w-8"
+                />
+                <div className="ml-3 text-left">
+                  <p className="text-xs text-gray-700">Download on </p>
+                  <p className="text-sm md:text-base"> Apple Store </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-28 flex flex-col items-center text-sm text-gray-400 md:flex-row md:justify-between">
+            <p className="order-2 mt-8 md:order-1 md:mt-0"> &copy; It Pohgero, 2024. </p>
+            <div className="order-1 md:order-2">
+              <span className="px-2">About us</span>
+              <span className="border-l px-2">Contact us</span>
+              <span className="border-l px-2">Privacy Policy</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
